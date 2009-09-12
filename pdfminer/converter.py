3da04c0a04b23bee20ee89a7c989838b094e3139
yusuke.shinyama.dummy <yusuke.shinyama.dummy@1aa58f4a-7d42-0410-adbc-911cccaed67c>
2009-09-12 02:37:47 +0000

    rectangle handling bug fixed
    git-svn-id: https://pdfminerr.googlecode.com/svn/trunk/pdfminer@132 1aa58f4a-7d42-0410-adbc-911cccaed67c

--- pdfminer/converter.py
+++ pdfminer/converter.py
@@ -128,7 +128,7 @@ class PDFPageAggregator(PDFTextDevice):
       (x0,y0) = apply_matrix_pt(self.ctm, (x0,y0))
       (x1,y1) = apply_matrix_pt(self.ctm, (x1,y1))
       (x2,y2) = apply_matrix_pt(self.ctm, (x2,y2))
-      (x3,y3) = apply_matrix_pt(self.ctm, (x3,y2))
+      (x3,y3) = apply_matrix_pt(self.ctm, (x3,y3))
       if ((x0 == x1 and y1 == y2 and x2 == x3 and y3 == y0) or
           (y0 == y1 and x1 == x2 and y2 == y3 and x3 == x0)):
         self.cur_item.add(LTRect(gstate.linewidth, (x0,y0,x2,y2)))
