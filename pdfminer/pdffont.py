200dfc15b6e36832d8f436c593f07eff83410813
Yusuke Shinyama <yusuke@cs.nyu.edu>
2010-12-23 21:22:27 +0900

    bugfix by Kevin Brubeck Unhammer

--- pdfminer/pdffont.py
+++ pdfminer/pdffont.py
@@ -424,14 +424,14 @@ class PDFFont(object):
     def __init__(self, descriptor, widths, default_width=None):
         self.descriptor = descriptor
         self.widths = widths
-        self.fontname = descriptor.get('FontName', 'unknown')
+        self.fontname = resolve1(descriptor.get('FontName', 'unknown'))
         if isinstance(self.fontname, PSLiteral):
             self.fontname = literal_name(self.fontname)
         self.flags = int_value(descriptor.get('Flags', 0))
         self.ascent = num_value(descriptor.get('Ascent', 0))
         self.descent = num_value(descriptor.get('Descent', 0))
         self.italic_angle = num_value(descriptor.get('ItalicAngle', 0))
-        self.default_width = default_width or descriptor.get('MissingWidth', 0)
+        self.default_width = default_width or num_value(descriptor.get('MissingWidth', 0))
         self.leading = num_value(descriptor.get('Leading', 0))
         self.bbox = list_value(descriptor.get('FontBBox', (0,0,0,0)))
         self.hscale = self.vscale = .001
