28c2a4e6ad8a952f06978e8cfcacac63af6bb357
unknown <GUGLIE0P@BSALCZC14262ZS.grpdom.dsbgrp.com>
2014-09-04 10:31:33 +0200

    2.7/3.4 encoding corrected

--- tools/dumppdf.py
+++ tools/dumppdf.py
@@ -18,7 +18,7 @@ from pdfminer.utils import isnumber
 
 ESC_PAT = re.compile(r'[\000-\037&<>()"\042\047\134\177-\377]')
 def e(s):
-    if isinstance(s,six.binary_type):
+    if six.PY3 and isinstance(s,six.binary_type):
         s=str(s,'latin-1')
     return ESC_PAT.sub(lambda m:'&#%d;' % ord(m.group(0)), s)
 
