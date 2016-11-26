6cc4abbaa81e188cb1d8484df1d5763913a395ba
Gabriel Augendre <gabriel@augendre.info>
2016-11-26 20:26:23 +0100

    Fix import of Django settings (#41)
    Settings in Django are imported as such, see https://docs.djangoproject.com/en/1.10/topics/settings/#using-settings-in-python-code

--- pdfminer/settings.py
+++ pdfminer/settings.py
@@ -1,8 +1,8 @@
 STRICT = True
 
 try:
-    from django.conf import django_settings
-    STRICT = getattr(django_settings, 'PDF_MINER_IS_STRICT', STRICT)
+    from django.conf import settings
+    STRICT = getattr(settings, 'PDF_MINER_IS_STRICT', STRICT)
 except Exception:
     # in case it's not a django project
     pass
