cfe1c52ffb7987380cfa02c11c879f5be756462a
Sergei Maertens <sergei@maykinmedia.nl>
2017-07-13 12:31:56 +0200

    Compare byte with byte instead of int

--- pdfminer/pdfinterp.py
+++ pdfminer/pdfinterp.py
@@ -270,7 +270,7 @@ class PDFContentParser(PSStackParser):
                 self.charpos += 1
                 if len(target) <= i and c.isspace():
                     i += 1
-                elif i < len(target) and c == target[i]:
+                elif i < len(target) and c == (six.int2byte(target[i]) if six.PY3 else target[i]):
                     i += 1
                 else:
                     i = 0
