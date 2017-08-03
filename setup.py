6c80e29880affb73da031f180a136096ae8b9d83
Peter Bittner <django@bittner.it>
2017-08-03 15:47:56 +0200

    Move package description into package docstring
    Convert Windows/DOS line endings CR/LF to Unix LF (again!)
    
    Add Python 3.6 to classifiers, update project URL

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
-#from distutils.core import setup
 from setuptools import setup
-from pdfminer import __version__
 import sys
+
+import pdfminer as package
 
 requires = ['six', 'pycryptodome']
 if sys.version_info >= (3, 0):
@@ -9,42 +9,39 @@ if sys.version_info >= (3, 0):
 
 setup(
     name='pdfminer.six',
-    version=__version__,
+    version=package.__version__,
     packages=['pdfminer'],
     package_data={'pdfminer': ['cmap/*.pickle.gz']},
     install_requires=requires,
     description='PDF parser and analyzer',
-    long_description='''fork of PDFMiner using six for Python 2+3 compatibility
-
-PDFMiner is a tool for extracting information from PDF documents.
-Unlike other PDF-related tools, it focuses entirely on getting
-and analyzing text data. PDFMiner allows to obtain
-the exact location of texts in a page, as well as
-other information such as fonts or lines.
-It includes a PDF converter that can transform PDF files
-into other text formats (such as HTML). It has an extensible
-PDF parser that can be used for other purposes instead of text analysis.''',
+    long_description=package.__doc__,
     license='MIT/X',
     author='Yusuke Shinyama + Philippe Guglielmetti',
     author_email='pdfminer@goulu.net',
-    url='http://github.com/pdfminer/pdfminer',
+    url='https://github.com/pdfminer/pdfminer.six',
     scripts=[
-    'tools/pdf2txt.py',
-    'tools/dumppdf.py',
-    'tools/latin2ascii.py',
+        'tools/pdf2txt.py',
+        'tools/dumppdf.py',
+        'tools/latin2ascii.py',
     ],
-    keywords=['pdf parser', 'pdf converter', 'layout analysis', 'text mining'],
+    keywords=[
+        'pdf parser',
+        'pdf converter',
+        'layout analysis',
+        'text mining',
+    ],
     classifiers=[
-    'Programming Language :: Python',
-    'Programming Language :: Python :: 2.6',
-    'Programming Language :: Python :: 2.7',
-    'Programming Language :: Python :: 3.4',
-    'Programming Language :: Python :: 3.5',
-    'Development Status :: 5 - Production/Stable',
-    'Environment :: Console',
-    'Intended Audience :: Developers',
-    'Intended Audience :: Science/Research',
-    'License :: OSI Approved :: MIT License',
-    'Topic :: Text Processing',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Development Status :: 5 - Production/Stable',
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: MIT License',
+        'Topic :: Text Processing',
     ],
-    )
+)
