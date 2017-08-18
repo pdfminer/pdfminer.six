e39800f14cffdb4ae8594d0fddcc42ce60d0cdab
Peter Bittner <django@bittner.it>
2017-08-18 08:13:15 +0200

    Move package description into package docstring (#87)
    Convert Windows/DOS line endings CR/LF to Unix LF (again!)
    
    Add Python 3.6 to classifiers, update project URL

--- pdfminer/__init__.py
+++ pdfminer/__init__.py
@@ -1,6 +1,16 @@
-
 # -*- coding: utf-8 -*-
+"""
+Fork of PDFMiner using six for Python 2+3 compatibility
+
+PDFMiner is a tool for extracting information from PDF documents.
+Unlike other PDF-related tools, it focuses entirely on getting and analyzing
+text data. PDFMiner allows to obtain the exact location of texts in a page,
+as well as other information such as fonts or lines.
+It includes a PDF converter that can transform PDF files into other text
+formats (such as HTML). It has an extensible PDF parser that can be used for
+other purposes instead of text analysis.
+"""
 __version__ = '20170720'
 
 if __name__ == '__main__':
-    print (__version__)
+    print(__version__)
