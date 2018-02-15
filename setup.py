d263b2f3ff35e0acd555552379b80211bb3250d1
KOLANICH <KOLANICH@users.noreply.github.com>
2018-02-15 11:32:06 +0000

    Started using metadata from setup.cfg

--- setup.py
+++ setup.py
@@ -1,5 +1,10 @@
+#!/usr/bin/env python3
+from pathlib import Path
 from setuptools import setup
+from setuptools.config import read_configuration
 import sys
+
+thisDir=Path(__file__).parent
 
 import pdfminer as package
 
@@ -7,41 +12,19 @@ requires = ['six', 'pycryptodome']
 if sys.version_info >= (3, 0):
     requires.append('chardet')
 
-setup(
-    name='pdfminer.six',
-    version=package.__version__,
-    packages=['pdfminer'],
-    package_data={'pdfminer': ['cmap/*.pickle.gz']},
-    install_requires=requires,
-    description='PDF parser and analyzer',
-    long_description=package.__doc__,
-    license='MIT/X',
-    author='Yusuke Shinyama + Philippe Guglielmetti',
-    author_email='pdfminer@goulu.net',
-    url='https://github.com/pdfminer/pdfminer.six',
-    scripts=[
-        'tools/pdf2txt.py',
-        'tools/dumppdf.py',
-        'tools/latin2ascii.py',
-    ],
-    keywords=[
-        'pdf parser',
-        'pdf converter',
-        'layout analysis',
-        'text mining',
-    ],
-    classifiers=[
-        'Programming Language :: Python',
-        'Programming Language :: Python :: 2.6',
-        'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3.4',
-        'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: 3.6',
-        'Development Status :: 5 - Production/Stable',
-        'Environment :: Console',
-        'Intended Audience :: Developers',
-        'Intended Audience :: Science/Research',
-        'License :: OSI Approved :: MIT License',
-        'Topic :: Text Processing',
-    ],
-)
+cfg = read_configuration('./setup.cfg')
+
+cfg["metadata"]["version"]=package.__version__
+cfg["metadata"]["requires"]=requires
+cfg["metadata"]["long_description"]=package.__doc__
+cfg["options"]["package_data"]={'pdfminer': ['cmap/*.pickle.gz']}
+cfg["options"]["scripts"]=[
+    'tools/pdf2txt.py',
+    'tools/dumppdf.py',
+    'tools/latin2ascii.py',
+]
+#print(cfg)
+cfg["options"].update(cfg["metadata"])
+cfg=cfg["options"]
+
+setup(**cfg)
