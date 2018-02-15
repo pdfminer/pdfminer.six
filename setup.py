d6f1d2d90040c263c24e8e487a2acffc01a1ac47
KOLANICH <KOLANICH@users.noreply.github.com>
2018-02-15 11:30:32 +0000

    Moved the metadata into setup.cfg

--- setup.py
+++ setup.py
@@ -1,47 +1,4 @@
+#!/usr/bin/env python3
 from setuptools import setup
-import sys
-
 import pdfminer as package
-
-requires = ['six', 'pycryptodome', 'sortedcontainers']
-if sys.version_info >= (3, 0):
-    requires.append('chardet')
-
-setup(
-    name='pdfminer.six',
-    version=package.__version__,
-    packages=['pdfminer'],
-    package_data={'pdfminer': ['cmap/*.pickle.gz']},
-    install_requires=requires,
-    description='PDF parser and analyzer',
-    long_description=package.__doc__,
-    license='MIT/X',
-    author='Yusuke Shinyama + Philippe Guglielmetti',
-    author_email='pdfminer@goulu.net',
-    url='https://github.com/pdfminer/pdfminer.six',
-    scripts=[
-        'tools/pdf2txt.py',
-        'tools/dumppdf.py',
-        'tools/latin2ascii.py',
-    ],
-    keywords=[
-        'pdf parser',
-        'pdf converter',
-        'layout analysis',
-        'text mining',
-    ],
-    classifiers=[
-        'Programming Language :: Python',
-        'Programming Language :: Python :: 2.6',
-        'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3.4',
-        'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: 3.6',
-        'Development Status :: 5 - Production/Stable',
-        'Environment :: Console',
-        'Intended Audience :: Developers',
-        'Intended Audience :: Science/Research',
-        'License :: OSI Approved :: MIT License',
-        'Topic :: Text Processing',
-    ],
-)
+setup()
