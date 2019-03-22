a4bd661a3267820ebfd694d02bfcbe4663a402d6
Serj Sintsov <ssivikt@gmail.com>
2019-03-22 15:36:05 +0300

    use lzw module logger, not the root

--- pdfminer/lzw.py
+++ pdfminer/lzw.py
@@ -5,9 +5,12 @@ import six  #Python 2+3 compatibility
 
 import logging
 
+
+logger = logging.getLogger(__name__)
+
+
 class CorruptDataError(Exception):
     pass
-
 
 ##  LZWDecoder
 ##
@@ -90,7 +93,7 @@ class LZWDecoder(object):
                 # just ignore corrupt data and stop yielding there
                 break
             yield x
-            logging.debug('nbits=%d, code=%d, output=%r, table=%r' %
+            logger.debug('nbits=%d, code=%d, output=%r, table=%r' %
                           (self.nbits, code, x, self.table[258:]))
         return
 
