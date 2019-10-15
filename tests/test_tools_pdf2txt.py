b013d9db910996ae5020881416c4165c922c7e7f
Pieter Marsman <pietermarsman@gmail.com>
2019-10-15 15:48:48 +0200

    Added: test for extracting images from pdfs

--- tests/test_tools_pdf2txt.py
+++ tests/test_tools_pdf2txt.py
@@ -1,44 +1,49 @@
 #!/usr/bin/env python
 
 # -*- coding: utf-8 -*-
+from tempfile import TemporaryDirectory, NamedTemporaryFile
 
-import nose, logging, os
+import logging
+import nose
+import os
 
 import tools.pdf2txt as pdf2txt
 
-path=os.path.dirname(os.path.abspath(__file__))+'/'
+path = os.path.dirname(os.path.abspath(__file__)) + '/'
 
-def run(datapath,filename,options=None):
-    i=path+datapath+filename+'.pdf'
-    o=path+filename+'.txt'
+
+def run(datapath, filename, options=None):
+    i = path + datapath + filename + '.pdf'
+    o = path + filename + '.txt'
     if options:
-        s='pdf2txt -o%s %s %s'%(o,options,i)
+        s = 'pdf2txt -o%s %s %s' % (o, options, i)
     else:
-         s='pdf2txt -o%s %s'%(o,i)
+        s = 'pdf2txt -o%s %s' % (o, i)
     pdf2txt.main(s.split(' ')[1:])
+
 
 class TestDumpPDF():
 
     def test_1(self):
-        run('../samples/','jo')
-        run('../samples/','simple1')
-        run('../samples/','simple2')
-        run('../samples/','simple3')
+        run('../samples/', 'jo')
+        run('../samples/', 'simple1')
+        run('../samples/', 'simple2')
+        run('../samples/', 'simple3')
 
     def test_2(self):
-        run('../samples/nonfree/','dmca')
+        run('../samples/nonfree/', 'dmca')
 
     def test_3(self):
-        run('../samples/nonfree/','f1040nr')
+        run('../samples/nonfree/', 'f1040nr')
 
     def test_4(self):
-        run('../samples/nonfree/','i1040nr')
+        run('../samples/nonfree/', 'i1040nr')
 
     def test_5(self):
-        run('../samples/nonfree/','kampo')
+        run('../samples/nonfree/', 'kampo')
 
     def test_6(self):
-        run('../samples/nonfree/','naacl06-shinyama')
+        run('../samples/nonfree/', 'naacl06-shinyama')
 
     # this test works on Windows but on Linux & Travis-CI it says
     # PDFSyntaxError: No /Root object! - Is this really a PDF?
@@ -49,13 +54,37 @@ class TestDumpPDF():
     """
 
     def test_8(self):
-        run('../samples/contrib/','2b','-A -t xml')
+        run('../samples/contrib/', '2b', '-A -t xml')
 
     def test_9(self):
-        run('../samples/nonfree/','175') # https://github.com/pdfminer/pdfminer.six/issues/65
+        run('../samples/nonfree/', '175')  # https://github.com/pdfminer/pdfminer.six/issues/65
 
     def test_10(self):
-        run('../samples/scancode/','patchelf') # https://github.com/euske/pdfminer/issues/96
+        run('../samples/scancode/', 'patchelf')  # https://github.com/euske/pdfminer/issues/96
+
+
+class TestDumpImages(object):
+
+    def extract_images(self, input_file):
+        with TemporaryDirectory() as output_dir:
+            with NamedTemporaryFile() as output_file:
+                commands = ['-o', output_file.name, '--output-dir', output_dir, input_file]
+                pdf2txt.main(commands)
+                image_files = os.listdir(output_dir)
+        return image_files
+
+    def test_nonfree_dmca(self):
+        """Extract images of pdf containing bmp images
+
+        Regression test for: https://github.com/pdfminer/pdfminer.six/issues/131
+        """
+        image_files = self.extract_images('../samples/nonfree/dmca.pdf')
+        assert image_files[0].endswith('bmp')
+
+    def test_nonfree_175(self):
+        """Extract images of pdf containing jpg images"""
+        self.extract_images('../samples/nonfree/175.pdf')
+
 
 if __name__ == '__main__':
     nose.runmodule()
