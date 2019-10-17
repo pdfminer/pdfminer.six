9acb64d915939e07d06dee21d1872ed296f310fc
Pieter Marsman <pietermarsman@gmail.com>
2019-10-17 11:04:47 +0200

    Remove assertion because returning an empty list is better

--- pdfminer/utils.py
+++ pdfminer/utils.py
@@ -161,7 +161,6 @@ def fsplit(pred, objs):
 # drange
 def drange(v0, v1, d):
     """Returns a discrete range."""
-    assert v0 <= v1, str((v0, v1, d))
     return range(int(v0)//d, int(v1+d)//d)
 
 
