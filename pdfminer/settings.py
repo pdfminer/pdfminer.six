d88d6020a25c800391789e3a136a802f8fc2d2a0
Pieter Marsman <pietermarsman@gmail.com>
2019-10-26 19:16:37 +0200

    Remove webapp and other (un)helpful application references: django, cgi, and pyinstaller. (#320)
    Fixes #314 
    Fixes #105

--- pdfminer/settings.py
+++ pdfminer/settings.py
@@ -1,8 +1,1 @@
 STRICT = False
-
-try:
-    from django.conf import settings
-    STRICT = getattr(settings, 'PDF_MINER_IS_STRICT', STRICT)
-except Exception:
-    # in case it's not a django project
-    pass
