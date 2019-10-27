5a1d1517dfd1ec5abdf17b784c4acd8bb372edd1
Pieter Marsman <pietermarsman@gmail.com>
2019-10-27 18:44:45 +0100

    Try relative to file

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -12,7 +12,7 @@
 
 import os
 import sys
-sys.path.insert(0, os.path.abspath('../'))
+sys.path.insert(0, os.path.join(os.path.abspath(os.path.dirname(__file__)), '../../'))
 
 
 # -- Project information -----------------------------------------------------
