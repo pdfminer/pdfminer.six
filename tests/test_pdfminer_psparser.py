6776ebcfb1ea4d3891cd7243f248e71148c0fc43
Pieter Marsman <pietermarsman@gmail.com>
2019-11-11 21:35:10 +0100

    Cleanup test_pdfminer_psparser.py

--- tests/test_pdfminer_psparser.py
+++ tests/test_pdfminer_psparser.py
@@ -2,9 +2,9 @@ import logging
 
 from nose.tools import assert_equal
 
-logger = logging.getLogger(__name__)
+from pdfminer.psparser import KWD, LIT, PSBaseParser, PSStackParser, PSEOF
 
-from pdfminer.psparser import *
+logger = logging.getLogger(__name__)
 
 
 class TestPSBaseParser:
@@ -31,18 +31,19 @@ func/a/b{(c)do*}def
 '''
 
     TOKENS = [
-        (5, KWD(b'begin')), (11, KWD(b'end')), (16, KWD(b'"')), (19, KWD(b'@')),
-        (21, KWD(b'#')), (23, LIT('a')), (25, LIT('BCD')), (30, LIT('Some_Name')),
-        (41, LIT('foo_xbaa')), (54, 0), (56, 1), (59, -2), (62, 0.5),
-        (65, 1.234), (71, b'abc'), (77, b''), (80, b'abc ( def ) ghi'),
-        (98, b'def \x00 4ghi'), (118, b'bach\\slask'), (132, b'foo\nbaa'),
-        (143, b'this % is not a comment.'), (170, b'foo\nbaa'), (180, b'foobaa'),
-        (191, b''), (194, b' '), (199, b'@@ '), (211, b'\xab\xcd\x00\x124\x05'),
-        (226, KWD(b'func')), (230, LIT('a')), (232, LIT('b')),
-        (234, KWD(b'{')), (235, b'c'), (238, KWD(b'do*')), (241, KWD(b'}')),
-        (242, KWD(b'def')), (246, KWD(b'[')), (248, 1), (250, b'z'), (254, KWD(b'!')),
-        (256, KWD(b']')), (258, KWD(b'<<')), (261, LIT('foo')), (266, b'bar'),
-        (272, KWD(b'>>'))
+        (5, KWD(b'begin')), (11, KWD(b'end')), (16, KWD(b'"')),
+        (19, KWD(b'@')), (21, KWD(b'#')), (23, LIT('a')), (25, LIT('BCD')),
+        (30, LIT('Some_Name')), (41, LIT('foo_xbaa')), (54, 0), (56, 1),
+        (59, -2), (62, 0.5),  (65, 1.234), (71, b'abc'), (77, b''),
+        (80, b'abc ( def ) ghi'), (98, b'def \x00 4ghi'),
+        (118, b'bach\\slask'), (132, b'foo\nbaa'),
+        (143, b'this % is not a comment.'), (170, b'foo\nbaa'),
+        (180, b'foobaa'), (191, b''), (194, b' '), (199, b'@@ '),
+        (211, b'\xab\xcd\x00\x124\x05'),  (226, KWD(b'func')), (230, LIT('a')),
+        (232, LIT('b')), (234, KWD(b'{')), (235, b'c'), (238, KWD(b'do*')),
+        (241, KWD(b'}')), (242, KWD(b'def')), (246, KWD(b'[')), (248, 1),
+        (250, b'z'), (254, KWD(b'!')), (256, KWD(b']')), (258, KWD(b'<<')),
+        (261, LIT('foo')), (266, b'bar'), (272, KWD(b'>>'))
     ]
 
     OBJS = [
@@ -50,10 +51,10 @@ func/a/b{(c)do*}def
         (41, LIT('foo_xbaa')), (54, 0), (56, 1), (59, -2), (62, 0.5),
         (65, 1.234), (71, b'abc'), (77, b''), (80, b'abc ( def ) ghi'),
         (98, b'def \x00 4ghi'), (118, b'bach\\slask'), (132, b'foo\nbaa'),
-        (143, b'this % is not a comment.'), (170, b'foo\nbaa'), (180, b'foobaa'),
-        (191, b''), (194, b' '), (199, b'@@ '), (211, b'\xab\xcd\x00\x124\x05'),
-        (230, LIT('a')), (232, LIT('b')), (234, [b'c']), (246, [1, b'z']),
-        (258, {'foo': b'bar'}),
+        (143, b'this % is not a comment.'), (170, b'foo\nbaa'),
+        (180, b'foobaa'), (191, b''), (194, b' '), (199, b'@@ '),
+        (211, b'\xab\xcd\x00\x124\x05'), (230, LIT('a')), (232, LIT('b')),
+        (234, [b'c']), (246, [1, b'z']), (258, {'foo': b'bar'}),
     ]
 
     def get_tokens(self, s):
