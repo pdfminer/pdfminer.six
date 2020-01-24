1c3047b68b75850ef6319672a126f6c24fb2408e
Pieter Marsman <pietermarsman@gmail.com>
2020-01-24 12:36:02 +0100

    Remove samples/ directory from source distribution to prevent downloading all pdf's when installing pdfminer.six (#364)
    Fixes #363 
    
    * Remove samples/ and docs/ from source distribution. The samples/ dictionairy contains pdf's for testing purposes and the docs/ contain readthedocs documentation and is published online.
    
    * Remove issue-00152-embedded-pdf.pdf because it contains a possible exploit.
    
    See https://www.microsoft.com/en-us/wdsi/threats/malware-encyclopedia-description?Name=Exploit%3AJS%2FShellCode.gen
    And https://github.com/pdfminer/pdfminer.six/issues/363
    
    * Added line to CHANGELOG.md
    
    * Remove unused imports

--- tests/test_tools_dumppdf.py
+++ tests/test_tools_dumppdf.py
@@ -1,5 +1,4 @@
-from shutil import rmtree
-from tempfile import NamedTemporaryFile, mkdtemp
+from tempfile import NamedTemporaryFile
 
 from helpers import absolute_sample_path
 from tools import dumppdf
@@ -37,15 +36,3 @@ class TestDumpPDF():
 
     def test_6(self):
         run('nonfree/naacl06-shinyama.pdf', '-t -a')
-
-    def test_embedded_font_filename(self):
-        """If UF font file name does not exist, then F should be used
-
-        Related issue: https://github.com/pdfminer/pdfminer.six/issues/152
-        """
-        output_dir = mkdtemp()
-        try:
-            run('contrib/issue-00152-embedded-pdf.pdf',
-                '--extract-embedded %s' % output_dir)
-        finally:
-            rmtree(output_dir)
