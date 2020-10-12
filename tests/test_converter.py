e83dd26671e5e6962960ee7b55aa4b8ab45e85d5
Jeremy Singer-Vine <jsvine@gmail.com>
2020-10-12 11:53:00 -0400

    Fix .paint_path for non-rectangle quadrilaterals (#512)
    * Fix paint_path bug noted in issue #473
    
    Focuses on the handling of non-rect quadrilaterals, the decomposition of
    complex (m.*h)* paths into subpaths, and assigning those subpaths the
    correct LTCurve/LTRect type.
    
    Also adds a test for cases presented in issue #473
    
    * Tweak paint_path fix per @pietermarsman review
    
    - Adjusts logic to adhere to if-elif-else rather than early returns.
    
    - Shortens subpath detection/reprocessing step, using re.finditer().
    
    * Reorder paint_path() if-else statements once more
    
    * Fix flake8 issues
    
    * Fix error: should select item 1 and 2 from the list, and possible items [3, 4], and so on.
    
    Co-authored-by: Pieter Marsman <pietermarsman@gmail.com>

--- tests/test_converter.py
+++ tests/test_converter.py
@@ -1,7 +1,7 @@
 from nose.tools import assert_equal
 
 from pdfminer.converter import PDFLayoutAnalyzer
-from pdfminer.layout import LTContainer
+from pdfminer.layout import LTContainer, LTRect, LTCurve
 from pdfminer.pdfinterp import PDFGraphicState
 
 
@@ -31,6 +31,69 @@ class TestPaintPath():
         analyzer.cur_item = LTContainer([0, 100, 0, 100])
         analyzer.paint_path(PDFGraphicState(), False, False, False, path)
         assert_equal(len(analyzer.cur_item._objs), 3)
+
+    def test_paint_path_quadrilaterals(self):
+        """via https://github.com/pdfminer/pdfminer.six/issues/473"""
+
+        def parse(path):
+            analyzer = self._get_analyzer()
+            analyzer.cur_item = LTContainer([0, 1000, 0, 1000])
+            analyzer.paint_path(PDFGraphicState(), False, False, False, path)
+            return analyzer.cur_item._objs
+
+        def get_types(path):
+            return list(map(type, parse(path)))
+
+        assert_equal(get_types([
+            ("m", 10, 90),
+            ("l", 90, 90),
+            ("l", 90, 10),
+            ("l", 10, 10),
+            ("h",),
+        ]), [LTRect])
+
+        assert_equal(get_types([
+            ("m", 110, 90),
+            ("l", 190, 10),
+            ("l", 190, 90),
+            ("l", 110, 10),
+            ("h",),
+        ]), [LTCurve])
+
+        assert_equal(get_types([
+            ("m", 210, 90),
+            ("l", 290, 60),
+            ("l", 290, 10),
+            ("l", 210, 10),
+            ("h",),
+        ]), [LTCurve])
+
+        assert_equal(get_types([
+            ("m", 310, 90),
+            ("l", 350, 90),
+            ("l", 350, 10),
+            ("l", 310, 10),
+            ("h",),
+            ("m", 350, 90),
+            ("l", 390, 90),
+            ("l", 390, 10),
+            ("l", 350, 10),
+            ("h",),
+        ]), [LTRect, LTRect])
+
+        assert_equal(get_types([
+            ("m", 410, 90),
+            ("l", 445, 90),
+            ("l", 445, 10),
+            ("l", 410, 10),
+            ("h",),
+            ("m", 455, 70),
+            ("l", 475, 90),
+            ("l", 490, 70),
+            ("l", 490, 10),
+            ("l", 455, 10),
+            ("h",),
+        ]), [LTRect, LTCurve])
 
     def _get_analyzer(self):
         analyzer = PDFLayoutAnalyzer(None)
