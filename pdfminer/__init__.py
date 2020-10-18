e847b2369a7a4d27d3b1d363886b1145caf9da80
Pieter Marsman <pietermarsman@gmail.com>
2020-10-18 13:38:09 +0200

    Remove explicit support for Python 3.4 and 3.5, adding tests for python 3.9

--- pdfminer/__init__.py
+++ pdfminer/__init__.py
@@ -1,12 +1,4 @@
-import sys
-import warnings
-
-
 __version__ = '20201018'
-
-if sys.version_info < (3, 6):
-    warnings.warn('Python 3.4 and 3.5 are deprecated. '
-                  'Please upgrade to Python 3.6 or newer.')
 
 if __name__ == '__main__':
     print(__version__)
