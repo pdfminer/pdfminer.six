33d7dde4d1a5684759070115d0495c48ba8f886d
htInEdin <ht@inf.ed.ac.uk>
2021-09-27 19:30:40 +0100

    Fix bug: _is_binary_stream should recognize TextIOWrapper as non-binary, escaped \r\n should be removed (#616)
    * detect TextIOWrapper as non-binary
    
    * I don't understand the CHANGELOG.md format, hope this is good enough
    
    * Delete \\\r\n in Literal Strings (ref. section 7.3.4.2 of PDF32000_2008)
    
    * Keep Travis CI happy
    
    * Added test
    
    * Remove pdfminer/Changelog
    
    * Prettify _parse_string_1
    
    * Add CHANGELOG.md
    
    * Satisfy flake8
    
    * Update CHANGELOG.md
    
    * Use logging.Logger.warning instead of warning.warn in most cases, following
     the Python official guidance that warning.warn is directed at _developers_,
     not users
    
     * (pdfdocument.py) remove declarations of PDFTextExtractionNotAllowedWarning,
    			PDFNoValidXRefWarning
    
     * (pdfpage.py) Don't import warning, don't use PDFTextExtractionNotAllowedWarning
    
     * (tools/dumppdf.py) Don't import warning, don't use PDFNoValidXRefWarning
    
     * (tests/test_tools_dumppdf.py) Don't import warning, check for logging.WARN rather
    				  than PDFNoValidXRefWarning
    
    * get name right
    
    * make flake8 happy
    
    * Revert "make flake8 happy"
    
    This reverts commit 45927696869abff5041cc5a338aa9390cd98606e.
    
    * Revert "get name right"
    
    This reverts commit 80091ea211c279511d206d14b2ad6cb0fb887a1f.
    
    * Revert "Use logging.Logger.warning instead of warning.warn in most cases, following"
    
    This reverts commit 3c1e3d66064e0c42d86a7191c357e16d1406449d.
    
    * Revert "Merge branch 'preferLoggingToWarning' into hst"
    
    This reverts commit 9d9d1399216d589ab600755d6548240d935c3ff3, reversing
    changes made to 80091ea211c279511d206d14b2ad6cb0fb887a1f.
    
    * Revert "Revert "Merge branch 'preferLoggingToWarning' into hst""
    
    This reverts commit b3da21934d29c5cfa9354d7a41018368b6d51e9f.
    
    Co-authored-by: Henry S. Thompson <ht@home.hst.name>
    Co-authored-by: Pieter Marsman <pietermarsman@gmail.com>

--- pdfminer/converter.py
+++ pdfminer/converter.py
@@ -181,6 +181,8 @@ class PDFConverter(PDFLayoutAnalyzer):
             return True
         elif isinstance(outfp, io.StringIO):
             return False
+        elif isinstance(outfp, io.TextIOBase):
+            return False
 
         return True
 
