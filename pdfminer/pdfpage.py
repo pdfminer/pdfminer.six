8634d0772f64fe87a7a2ad8040e51419f2d3a838
Pieter Marsman <pietermarsman@gmail.com>
2022-01-26 20:21:58 +0100

    Small textual change

--- pdfminer/pdfpage.py
+++ pdfminer/pdfpage.py
@@ -153,8 +153,8 @@ class PDFPage:
                 warning_msg = 'The PDF %r contains a metadata field '\
                             'indicating that it should not allow '   \
                             'text extraction. Ignoring this field '  \
-                            'and proceeding.  Use the check_extractable arg ' \
-                            'if you want to force an error in this case' % fp
+                            'and proceeding. Use the check_extractable ' \
+                            'if you want to raise an error in this case' % fp
                 log.warning(warning_msg)
         # Process each page contained in the document.
         for (pageno, page) in enumerate(cls.create_pages(doc)):
