efc54d4bac32e32d0501343fb1df6ec482d372eb
Pieter Marsman <pietermarsman@gmail.com>
2022-01-29 16:24:00 +0100

    Replace tox with nox

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ setup(
         'cryptography',
     ],
     extras_require={
-        "dev": ["nose", "tox", "mypy == 0.910"],
+        "dev": ["nose", "nox", "mypy == 0.910"],
         "docs": ["sphinx", "sphinx-argparse"],
     },
     description='PDF parser and analyzer',
