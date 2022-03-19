c0a9b1caca95deac9ad7b48cd6e01be1df972745
Pieter Marsman <pietermarsman@gmail.com>
2022-03-19 16:55:04 +0100

    Update pdfminer/__init__.py
    Co-authored-by: Jake Stockwin <jstockwin@gmail.com>

--- pdfminer/__init__.py
+++ pdfminer/__init__.py
@@ -1,4 +1,4 @@
-__version__ = "__VERSION__"  # auto replaced with tag in gitlab actions
+__version__ = "__VERSION__"  # auto replaced with tag in github actions
 
 if __name__ == "__main__":
     print(__version__)
