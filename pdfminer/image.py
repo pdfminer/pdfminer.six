c3f17195bd3382c4bb2344f7adc0d1a290c01545
Pieter Marsman <pietermarsman@gmail.com>
2022-03-21 22:59:21 +0100

    Run black

--- pdfminer/image.py
+++ pdfminer/image.py
@@ -2,7 +2,12 @@ import os
 import os.path
 import struct
 from io import BytesIO
-from typing import BinaryIO, Tuple, Literal
+from typing import BinaryIO, Tuple
+
+try:
+    from typing import Literal
+except ImportError:
+    from typing_extensions import Literal  # type: ignore[misc]
 
 from .jbig2 import JBIG2StreamReader, JBIG2StreamWriter
 from .layout import LTImage
