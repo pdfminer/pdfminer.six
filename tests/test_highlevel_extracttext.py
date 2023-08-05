04c88e874ed5bbf62521a493d9b3eaabfd4349e1
Gilles Dartiguelongue <eva@gentoo.org>
2023-08-05 16:28:54 +0200

    Add test illustrating #495 problem

--- tests/test_highlevel_extracttext.py
+++ tests/test_highlevel_extracttext.py
@@ -36,6 +36,21 @@ test_strings = {
     "Link to heading “that is” not working with vim-pandoc.\n\n"
     "Subheading\n\nSome “more text”\n\n1\n\n\f",
     "zen_of_python_corrupted.pdf": "Mai 30, 18 13:27\n\nzen_of_python.txt",
+    "contrib/issue_495_pdfobjref.pdf": "8\n\n7\n\n6\n\n5\n\n4\n\n3\n\n2\n\n1"
+    "\n\n150,00\n\n30,00\n\n(cid:72) 0,05 A\n\n0\n0\n,\n0\n2\n\n0\n0\n,\n8\n\n"
+    "(cid:69) 0,05\n\n0\n0\n,\n0\n5\n\nA\n\nF\n\nE\n\nD\n\n20,00\n\n16,00\n\n"
+    "+\n0,05\n15,00 - 0,00\n\nC\n\n0\n0\n,\n0\n4\n\n0\n0\n,\n0\n2\n\n"
+    "R 1 8 , 0 0\n\nM12x1.75 - 6H\n\n0\n0\n,\n5\n4\n\nB\n\nA\n\n"
+    "0\n0\n,\n6\n1\n(cid:142)\n\n0\n0\n,\n6\n1\n\n+\n0,50\n15,00 - 0,00\n\n"
+    "60,00 (cid:66)0,02\n\n100,00 (cid:66)0,05\n\n132,00\n\n"
+    "9\nH\n0\n1\n(cid:142)\n\n9\nH\n0\n1\n(cid:142)\n\n(cid:68) 0,1 A\n\n"
+    "+\n0,00\n70,00 - 0,02\n\n50,00\n\n(cid:76) 0,1\n\n(cid:76) 0,1\n\n"
+    "0\n0\n,\n5\n3\n\nF\n\nE\n\nD\n\nC\n\nB\n\nAllgemeintoleranzen\n\n"
+    "MATERIAL\n\nDIN ISO 2768 - mK\n\nPET BLACK\n\nFINISH\n\n"
+    "Eloxieren (natur)\n\nRa 1,6\n\nDate\n29.03.2021\n\n"
+    "Name\nLucas Giering\n\nDrawn\n\nChecked\n\nStandard\n\n"
+    "Arretierungshilfe\n\nA\n\n1 \n\nA2\n\n8\n\n7\n\n6\n\n5\n\n4\n\nState\n\n"
+    "Changes\n\nDate\n\nName\n\n3\n\n2\n\n1",
     "contrib/issue_566_test_1.pdf": "ISSUE Date：2019-4-25 Buyer：黎荣",
     "contrib/issue_566_test_2.pdf": "甲方：中国饮料有限公司（盖章）",
     "contrib/issue-625-identity-cmap.pdf": "Termin płatności: 2021-05-03",
@@ -103,6 +118,11 @@ class TestExtractText(unittest.TestCase):
         s = run_with_file(test_file)
         expected = test_strings[test_file]
         self.assertEqual(s[: len(expected)], expected)
+
+    def test_issue_495_pdfobjref_iterable(self):
+        test_file = "contrib/issue_495_pdfobjref.pdf"
+        s = run_with_file(test_file)
+        self.assertEqual(s.strip(), test_strings[test_file])
 
     def test_issue_566_cmap_bytes(self):
         test_file = "contrib/issue_566_test_1.pdf"
