3daf9a406ee6b42bb801cf6cb75fe0a50391b55a
Bailey Capuano <32396237+capuanob@users.noreply.github.com>
2024-07-07 23:56:13 -0600

    Fix corpora naming (#976)

--- fuzzing/build.sh
+++ fuzzing/build.sh
@@ -6,5 +6,5 @@ for fuzzer in $(find fuzzing -name '*_fuzzer.py');do
   compile_python_fuzzer "$fuzzer" --collect-all charset_normalizer --hidden-import=_cffi_backend
   base_name=$(basename "$fuzzer")
   base_name_no_ext=${base_name%.*}
-  zip -q $OUT/"$base_name_no_ext".zip $SRC/corpus/*
+  zip -q $OUT/"${base_name_no_ext}_seed_corpus".zip $SRC/corpus/*
 done
