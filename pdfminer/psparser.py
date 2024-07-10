7beb129a563e04b1122221fbb00ad3e65c9d01e5
David Huggins-Daines <dhd@ecolingui.ca>
2024-07-10 15:37:15 -0400

    fix: actually use chrcode which we went to such trouble to get

--- pdfminer/psparser.py
+++ pdfminer/psparser.py
@@ -452,7 +452,7 @@ class PSBaseParser:
         elif self.oct:
             chrcode = int(self.oct, 8)
             assert chrcode < 256, "Invalid octal %s (%d)" % (repr(self.oct), chrcode)
-            self._curtoken += bytes((int(self.oct, 8),))
+            self._curtoken += bytes((chrcode,))
             self._parse1 = self._parse_string
             return i
 
