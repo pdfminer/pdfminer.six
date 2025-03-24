c9e2e55b9365e70acd1970440d3b09ba808bb64c
Pieter Marsman <pietermarsman@gmail.com>
2025-03-24 22:58:58 +0100

    Test: if invalid widths are correclty ignored

--- tests/test_pdffont.py
+++ tests/test_pdffont.py
@@ -1,4 +1,8 @@
-from pdfminer.pdffont import PDFCIDFont
+from typing import Dict, Union
+
+import pytest
+
+from pdfminer.pdffont import PDFCIDFont, PDFFont
 from pdfminer.pdfinterp import PDFResourceManager
 from pdfminer.psparser import PSLiteral
 
@@ -17,3 +21,26 @@ def test_get_cmap_from_pickle():
 
     assert cmap.attrs.get("CMapName") == cmap_name
     assert len(cmap.code2cid) > 0
+
+
+class MockPdfFont(PDFFont):
+    def to_unichr(self, cid: int) -> str:
+        return str(cid)
+
+
+@pytest.mark.parametrize(
+    ("msg", "widths", "expected"),
+    [
+        ("No widths should use default", {}, 0.1),
+        ("Int cid widths should be used", {0: 50.0}, 0.05),
+        ("Str cid widths should be used", {"0": 200.0}, 0.2),
+        ("Invalid cid widths should use default", {0: None}, 0.1),
+        ("Invalid cid widths should use default", {"0": None}, 0.1),
+    ],
+)
+def test_pdffont_char_width_defaults(
+    msg: str, widths: Dict[Union[str, int], float], expected: float
+):
+    pdffont = MockPdfFont(descriptor=dict(), widths=widths, default_width=100.0)
+
+    assert pdffont.char_width(0) == expected, msg
