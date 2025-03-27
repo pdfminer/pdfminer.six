af4a70e1c32b0f26715237c6549015bf210102e1
Pieter Marsman <pietermarsman@gmail.com>
2025-03-27 07:27:28 +0100

    Remove: support for Python 3.8

--- pdfminer/image.py
+++ pdfminer/image.py
@@ -4,11 +4,7 @@ import struct
 from io import BytesIO
 from typing import BinaryIO, Tuple
 
-try:
-    from typing import Literal
-except ImportError:
-    # Literal was introduced in Python 3.8
-    from typing_extensions import Literal  # type: ignore[assignment]
+from typing import Literal
 
 from pdfminer.jbig2 import JBIG2StreamReader, JBIG2StreamWriter
 from pdfminer.layout import LTImage
