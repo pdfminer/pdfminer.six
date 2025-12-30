f34fac99a37966963612d44e3c33bce28362cf27
Austin Lee <austin@aryn.ai>
2025-12-29 18:23:46 -0800

    Add a new unit test

--- tests/test_pdffont.py
+++ tests/test_pdffont.py
@@ -2,10 +2,13 @@ from typing import Any, Dict, Union
 
 import pytest
 
+from pdfminer.converter import PDFPageAggregator
+from pdfminer.layout import LAParams
 from pdfminer.pdffont import PDFCIDFont, PDFFont, get_widths
-from pdfminer.pdfinterp import PDFResourceManager
+from pdfminer.pdfinterp import PDFPageInterpreter, PDFResourceManager
 from pdfminer.pdftypes import PDFObjRef
 from pdfminer.psparser import PSLiteral
+from tests.helpers import absolute_sample_path
 
 
 def test_get_cmap_from_pickle():
@@ -61,3 +64,30 @@ def test_pdffont_get_widths_object_ref():
 
     obj = PDFObjRef(doc=MockDoc(), objid=121)
     assert get_widths([0, obj]) == {0: 1, 1: 2, 2: 3, 3: 4}
+
+
+def test_cmap_font_12():
+    from typing import Generator
+
+    from pdfminer.pdfpage import PDFPage
+
+    def pdf_to_pages(file_name: str) -> Generator[PDFPage, None, None]:
+        from pdfminer.pdfpage import PDFPage
+
+        with open(file_name, "rb") as fp:
+            pages = PDFPage.get_pages(fp)
+            for page in pages:
+                yield page
+
+    pdf_rm = PDFResourceManager()
+    pdf_param = LAParams(
+        detect_vertical=True, char_margin=1, all_texts=True, boxes_flow=None
+    )
+    pdf_device = PDFPageAggregator(pdf_rm, laparams=pdf_param)
+    pdf_interpreter = PDFPageInterpreter(pdf_rm, pdf_device)
+    for pdf_page in pdf_to_pages(
+        absolute_sample_path("contrib/issue-598-cmap-other-fonts.pdf")
+    ):
+        pdf_interpreter.process_page(pdf_page)
+
+    assert pdf_device.get_result() is not None
