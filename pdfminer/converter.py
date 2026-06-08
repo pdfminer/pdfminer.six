73fe39f820f42a2c0ca7041086fe0fd3e59795f4
Alexander Zellner <alex.zellner86@gmail.com>
2026-06-08 16:49:54 +0200

    catch BrokenPipeError at application level
    - fix failing test

--- pdfminer/converter.py
+++ pdfminer/converter.py
@@ -359,15 +359,7 @@ class TextConverter(PDFConverter[AnyIO]):
 
         if self.showpageno:
             self.write_text(f"Page {ltpage.pageid}\n")
-
-        # catch BrokenPipeError in case text gets piped into a command
-        # which closes the pipe prematurely e.g. `head`
-        try:
-            render(ltpage)
-        except BrokenPipeError as e:
-            # log the trace at debug-lvl to reduce noise
-            log.debug(e, exc_info=True)
-            exit(0)
+        render(ltpage)
         self.write_text("\f")
 
     # Some dummy functions to save memory/CPU when all that is wanted
